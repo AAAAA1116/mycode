f8f1852e8ccc42b2a868d209ef219b0c7ac86727
AAAAA1116 <3206446867@qq.com>
2025-08-04 21:40:04 +0800

    first_A

--- reserve_str.py
+++ reserve_str.py
@@ -4,3 +4,9 @@ def reserve_str(s):
     return reserve_str(s[1:]) + s[0]
 
 print(reserve_str("hello"))
+def reserve_str(s):
+    if len(s) <= 1:
+        return s
+    return reserve_str(s[1:]) + s[0]
+
+print(reserve_str("hello"))
